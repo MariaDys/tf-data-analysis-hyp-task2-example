0ab14278e9ab32fab1172517f223a816f25a7209
MariaDys <77514289+MariaDys@users.noreply.github.com>
2023-04-13 15:45:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,10 @@ from scipy.stats import ttest_ind
 
 chat_id = 734920047 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array, y: np.array) -> bool:
-    crit = 0.05
-    t, p = ttest_ind(x,y)
-    return (not(p < crit))
+import numpy as np
+from scipy.stats import f_oneway
+
+def solution(x: np.ndarray, y: np.ndarray) -> bool:
+    alpha = 0.05
+    _, p_value = f_oneway(x, y)
+    return p_value < alpha
