78e4ee9d79471168bcb51a05f0d75c89d7e02973
MariaDys <77514289+MariaDys@users.noreply.github.com>
2023-04-13 16:29:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import f_oneway
+from scipy.stats import ttest_ind
 chat_id = 734920047 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.ndarray, y: np.ndarray) -> bool:
     alpha = 0.05
-    _, p_value = f_oneway(x, y)
+    _, p_value = ttest_ind(x, y)
     return p_value < alpha
